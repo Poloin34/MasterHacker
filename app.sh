2e9e121bbd7faee1057967aa253a6ddc9a4f74c9
Poloin34 <poloin.gaetan@gmail.com>
2020-06-28 02:15:58 +0200

    All the gov hack are disponible !

--- app.sh
+++ app.sh
@@ -15,8 +15,6 @@ banner () {
 }
 
 gov () {
-
-
     govdb () {
         banner
         printf " 1) Access to the FBI databases\t2) Access to the CIA databases"
@@ -55,14 +53,14 @@ gov () {
         do
             echo -e "$line "
             sleep .05
-        done < fake.txt
+        done < victims.txt
 
         echo "${reset}Finished to breach into $agencie databases ! "
         printf "\n"
         echo "Wait a few seconds for identity deletion (Now worry, its to protect you).."
         printf "\n"
         printf "."; sleep 1; printf "."; sleep 1; printf "."; sleep 2; printf "."; sleep 2;
-        echo "\tDone ! Here how to connect :"
+        printf "\tDone ! Here how to connect :"
         printf "\n"
         sleep 5
         echo "${red}We have some trouble getting in.. check internet connection and cpu usage..${reset}"
@@ -76,10 +74,124 @@ gov () {
         exit
     }
     govfamous () {
-        printf "famous"
+        banner
+        printf " 1) Access Trump's Phone\t2) Access Trump's Laptop"
+        printf "\n"
+        printf " 3) Access Pence's Phone\t4) Access Pence's Laptop"
+        printf "\n"
+        printf " 5) Access Obama's Phone\t6) Access Obama's Laptop"
+        printf "\n"
+        printf " 7) Access White House security system (WARNING, CAN CAUSE A NUCLEAR WAR)"
+        printf "\n\n"
+        read -p "Which one do you want to access ? " govfamouschoice
+
+        if [ $govfamouschoice == 1 ]
+        then
+            people="TRUMP PHONE"
+        elif [ $govfamouschoice == 2 ]
+        then
+            people="TRUMP LAPTOP"
+        elif [ $govfamouschoice == 3 ]
+        then
+            people="PENCE PHONE"
+        elif [ $govfamouschoice == 4 ]
+        then
+            people="PENCE LAPTOP"
+        elif [ $govfamouschoice == 5 ]
+        then
+            people="OBAMA PHONE"
+        elif [ $govfamouschoice == 6 ]
+        then
+            people="OBAMA LAPTOP"
+        elif [ $govfamouschoice == 7 ]
+        then
+            people="WHITE HOUSE SECURITY"
+        fi
+
+        echo "${green}$people is fun"
+
+        while read line
+        do
+            echo -e "$line "
+            sleep .05
+        done < victims.txt
+
+        echo "${reset}Finished to breach into $people ! "
+        printf "\n"
+        echo "Wait a few seconds for identity deletion (Now worry, its to protect you).."
+        printf "\n"
+        printf "."; sleep 1; printf "."; sleep 1; printf "."; sleep 2; printf "."; sleep 2;
+        printf "\tDone ! Here how to connect :"
+        printf "\n"
+        sleep 5
+        echo "${red}We have some trouble getting in.. check internet connection and cpu usage..${reset}"
+        printf "\n"
+        echo "Retrying."
+        printf "."; sleep 1; printf "."; sleep 1; printf "."; sleep 2; printf "."; sleep 2;
+        sleep 15
+        printf "\n"
+        echo "${red}We have some trouble getting in.. check internet connection and cpu usage..${reset}"
+        xdg-open https://github.com/Poloin34/MasterHacker/victims/
+        exit
+
     }
     govconglo () {
-        printf "conglomerate"
+        banner
+        printf " 1) Get 100 random free items from Walmart\t2) Get 1000 random little things from Amazon"
+        printf "\n"
+        printf " 3) Get Amazon Prime for life\t4) Get ten 100% discount off at Amazon"
+        printf "\n"
+        printf " 5) Get Netflix free, for ever ! (Available for now)"
+        printf "\n\n"
+        read -p "Which one do you want to have ? " govconglochoice
+
+        if [ $govconglochoice == 1 ]
+        then
+            conglothings="100 RANDOM FREE WALMART ITEM"
+        elif [ $govconglochoice == 2 ]
+        then
+            conglothings="1000 RANDOM LITTLE AMAZON THINGS"
+        elif [ $govconglochoice == 3 ]
+        then
+            conglothings="LIFE TIME AMAZON PRIME"
+        elif [ $govconglochoice == 4 ]
+        then
+            conglothings="10x 100% DISCOUNT AMAZON COUPONS CODES"
+        elif [ $govconglochoice == 5 ]
+        then
+            conglothings="FREE NETFLIX LIFE TIME"
+        fi
+
+        echo "${green}$conglothings is fun"
+
+        while read line
+        do
+            echo -e "$line "
+            sleep .05
+        done < victims.txt
+
+        while read line
+        do
+            echo -e "$line "
+            sleep .007
+        done < victims.txt
+        echo "${reset}Finished to breach in to get your $conglothings ! "
+        printf "\n"
+        echo "Wait a few seconds for identity deletion (Now worry, its to protect you).."
+        printf "\n"
+        printf "."; sleep 1; printf "."; sleep 1; printf "."; sleep 2; printf "."; sleep 2;
+        printf "\tDone ! Here how to get them :"
+        printf "\n"
+        sleep 5
+        echo "${red}We have some trouble getting in.. check internet connection and cpu usage..${reset}"
+        printf "\n"
+        echo "Retrying."
+        printf "."; sleep 1; printf "."; sleep 1; printf "."; sleep 2; printf "."; sleep 2;
+        sleep 15
+        printf "\n"
+        echo "${red}We have some trouble getting in.. check internet connection and cpu usage..${reset}"
+        xdg-open https://github.com/Poloin34/MasterHacker/victims/
+        exit
     }
 
     banner
@@ -89,7 +201,7 @@ gov () {
     printf "\n"
     printf "2) Access to differents famous gov computers and phones"
     printf "\n"
-    printf "3) Exploit some breache in conglomerate website and app to get free items and 100% discount"
+    printf "3) Exploit some breache in conglomerate website/app to get free items and 100% discount"
     printf "\n\n"
     read -p "Which one do you want to see ? " govchoice
 
